35238526b1b924da1af67cd2fa4b771ca9133ca2
Rhaphie Costante <117087910+Rhaphie12@users.noreply.github.com>
2024-03-13 19:15:46 +0800

    gg

---
+++ config/connection.php
@@ -0,0 +1,11 @@
+<?php
+$serverName = "localhost";
+$userName = "root";
+$passWord = "";
+$database = "college_information";
+
+$connection = new mysqli($serverName, $userName, $passWord, $database);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
